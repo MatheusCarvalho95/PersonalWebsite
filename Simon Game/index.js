bcc887d2f5684dff2da6e7d967c2f982264c7ed5
Matheus Carvalho <matheus_carvalho2@hotmail.com>
2020-08-24 17:17:36 -0300

    Add Simon Game

---
+++ Simon Game/index.js
@@ -0,0 +1,79 @@
+var gamePattern =[];
+var buttonColours =["red","green","blue","yellow"];
+var userClickedPattern=[];
+
+var started=false;
+var level=0;
+
+$(document).keydown(function(){
+    if(!started){
+        $("#level-title").text("Level: " + level);
+        nextSequence();
+        started=true;
+    }
+})
+
+function nextSequence(){
+    console.log("next")
+    level++;
+    userClickedPattern =[];
+    $("#level-title").text("Level "+level);
+var randomNumber = Math.floor(Math.random() * 4); 
+var randomChosenColour = buttonColours[randomNumber]
+gamePattern.push(randomChosenColour)
+$("#"+ randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+}
+
+
+$(".btn").click(function (){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.lastIndexOf(userChosenColour));
+})
+
+
+
+function playSound(name){
+    var audio = new Audio("sounds/"+ name+".mp3");
+    audio.play();
+}
+
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColour).removeClass("pressed");
+    },100);
+}
+
+
+function checkAnswer(currentLevel){
+if (gamePattern[currentLevel]=== userClickedPattern[currentLevel]){
+    console.log("success");
+    if (userClickedPattern.length=== gamePattern.length){
+
+        setTimeout(function(){
+            nextSequence();
+        },1000);
+    }
+}else {
+    console.log("wrong");
+    var wrongAns = new Audio("sounds/wrong.mp3");
+    wrongAns.play()
+    $("body").addClass("game-over")
+    setTimeout(function(){
+        $("body").removeClass("game-over")
+    })
+    $("h1").text("Game Over, press any key to restart")
+    startOver()
+}
+}
+
+function startOver(){
+    level = 0;
+    gamePattern=[];
+    started= false;
+}
